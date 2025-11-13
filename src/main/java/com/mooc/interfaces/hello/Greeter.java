0ddb4cb0b578655c33707129b7fb50342f4289b8
Polaski <101113655+2Tough@users.noreply.github.com>
2025-11-14 00:19:30 +0300

    Initial commit

---
+++ src/main/java/com/mooc/interfaces/hello/Greeter.java
@@ -0,0 +1,5 @@
+package com.mooc.interfaces.hello;
+
+public interface Greeter {
+    String greet(String name);
+}
---
+++ src/main/java/com/mooc/interfaces/hello/HelloJavaAgain.java
@@ -0,0 +1,8 @@
+package com.mooc.interfaces.hello;
+
+public class HelloJavaAgain {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
